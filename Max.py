084a8b21c9b9772f08a2cf9d7cea8d6f9e7d8ed3
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-06 18:18:40 +0300

    Update Max.py

--- Max.py
+++ Max.py
@@ -5,7 +5,7 @@ os.system('termux-setup-storage')
 
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Jm")._login()
+	__import__("Dumpp32")._login()
 elif 'aarch' in arc:
 	__import__("Dumpp")._login()
 else:
