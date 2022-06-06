d90055fb9173944ac4864333d61a1cf3d99e5462
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-06 09:39:30 +0300

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -8,7 +8,7 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Jm")._login()
 elif 'aarch' in arc:
-	__import__("Dump")._login()
+	__import__("dm")._login()
 else:
 	exit(f' Unknow device machine {arc}')
 
